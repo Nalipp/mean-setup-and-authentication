6bc888e41ca607878e3e907171f9130652b15c16
Nalipp <nateplipp@gmail.com>
2017-06-21 13:57:48 +0900

    property binding between components example

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
+import { Message } from './message/message.model';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styles: [`
-      .author {
-        display: inline-block;
-        font-style: italic;
-        font-size: 12px;
-        width: 80%;
-      }
-      .config {
-        display: inline-block;
-        text-align: right;
-        font-size: 12px;
-        width: 19%;
-      }
-    `];
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  message = {
-    content: 'some content';    
-    author: 'nate';    
-  }
+  message: Message = new Message('some message', 'nate');
 }
