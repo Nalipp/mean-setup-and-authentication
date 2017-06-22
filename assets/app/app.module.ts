200415fbc32e7d7056c4f40048d77f6793c0ab90
Nalipp <nateplipp@gmail.com>
2017-06-22 09:33:18 +0900

    routing and server configuration

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
 import { MessageComponent } from "./messages/message.component";
+import { MessagesComponent } from "./messages/messages.component";
 import { MessageListComponent } from "./messages/message-list.component";
 import { MessageInputComponent } from "./messages/message-input.component";
+import { AuthenticationComponent } from "./auth/authentication.component";
+import { HeaderComponent } from "./header.component";
+import { routing } from "./app.routing";
 
 @NgModule({
     declarations: [
       MessageListComponent,
       MessageComponent,
       AppComponent,
-      MessageInputComponent
+      MessageInputComponent,
+      MessagesComponent,
+      AuthenticationComponent,
+      HeaderComponent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ assets/app/auth/authentication.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authentication',
+  templateUrl: `<h1>authentication</h1>`
+})
+
+export class AuthenticationComponent {
+
+}
