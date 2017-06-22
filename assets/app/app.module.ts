58645477ad90af70d53008bd72a0b48a51d0e43e
Nalipp <nateplipp@gmail.com>
2017-06-22 16:29:35 +0900

    connect backend with addMessage()

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from "./app.component";
 import { MessageComponent } from "./messages/message.component";
@@ -27,7 +28,13 @@ import { SigninComponent } from "./auth/signin.component";
       LogoutComponent,
       SignupComponent
     ],
-    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule],
+  imports: [
+    BrowserModule, 
+    FormsModule,
+    routing,
+    ReactiveFormsModule,
+    HttpModule
+  ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
